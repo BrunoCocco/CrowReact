95af7f5da020e575157e22dab7cc9d81ae4a1ccd
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-14 22:59:47 +0100

    agregando funciones a proyecto

--- src/layouts/Cuenta.jsx
+++ src/layouts/Cuenta.jsx
@@ -1,31 +1,39 @@
-import { useState } from "react";
+import {useState} from "react"
 import "./layouts.css";
 
-function Cuenta() {
-  const [invertido, setInvertido] = useState(80.40);
+function Cuenta({ invertido, setInvertido }) {
   const [stakeado, setStakeado] = useState(40.15);
 
   return (
-    <>
-      <article className="cuenta background">
-        <h1 className="mr">cuenta</h1>
-        <h3>salgo liquido: ${(invertido).toFixed(2)}</h3>
-        <h3>salgo stakeado: ${(stakeado).toFixed(2)}</h3>
-        <p>
-          <code>Total : ${(invertido + stakeado).toFixed(2)}</code>
-        </p>
-        <button
-  onClick={() => {
-    if (window.confirm("¿Estás seguro de invertir el total?")) {
-      setStakeado(stakeado + 80.4);
-      setInvertido(invertido - 80.4);
-    }
-  }}
->
-  Invertir total
-</button>
-      </article>
-    </>
+    <article className="cuenta background">
+      <h1 className="mr">cuenta</h1>
+      <h3>saldo líquido: ${invertido.toFixed(2)}</h3>
+      <h3>saldo stakeado: ${stakeado.toFixed(2)}</h3>
+      <p>
+        <code>Total : ${(invertido + stakeado).toFixed(2)}</code>
+      </p>
+
+      <button
+        onClick={() => {
+          if (window.confirm("¿Estás seguro de invertir el total?")) {
+            setStakeado(stakeado + invertido);
+            setInvertido(0);
+          }
+        }}
+      >
+        Invertir total
+      </button>
+       <button
+        onClick={() => {
+          if (window.confirm("¿Estás seguro de retirar el total?")) {
+            setStakeado(stakeado - stakeado);
+            setInvertido(invertido + stakeado);
+          }
+        }}
+      >
+        Invertir total
+      </button>
+    </article>
   );
 }
 
--- src/layouts/Proyecto.jsx
+++ src/layouts/Proyecto.jsx
@@ -1,19 +1,25 @@
-function Proyecto() {
+function Proyecto({ invertido, setInvertido }) {
   return (
     <>
       <article className="proyecto">
-        <h2>cuenta</h2>
-        <p>salgo invertido 80,45</p>
-        <p>saldo liquido 90.15</p>
-        <p>
-          total Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Temporibus fugit et, error nemo cum quisquam? Amet, ab. Ex repellendus
-          quas nam blanditiis earum, consequuntur fuga omnis distinctio ducimus
-          sed nostrum. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Exercitationem ipsam dolorum molestias quasi fugiat. Accusantium
-          commodi iusto placeat saepe ullam, corrupti cum dolore fugit itaque,
-          eius nesciunt numquam vel fuga.:
-        </p>
+        <div>
+          <button onClick={() => setInvertido(invertido + 10)}>
+            Agregar saldo
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              if (invertido <= 0) {
+                alert("No se puede retirar más saldo");
+              } else {
+                setInvertido(invertido - 10);
+              }
+            }}
+          >
+            Agregar saldo
+          </button>
+        </div>
       </article>
     </>
   );

5727e68d651bdd29d89171bf945844307b9b4ac4
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-14 22:29:17 +0100

    avanzando

--- src/layouts/Cuenta.jsx
+++ src/layouts/Cuenta.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
+import "./layouts.css";
+
 function Cuenta() {
+  const [invertido, setInvertido] = useState(80.40);
+  const [stakeado, setStakeado] = useState(40.15);
+
   return (
     <>
-      <article className="cuenta">
-        <h2>cuenta</h2>
-        <p>salgo invertido 80,45</p>
-        <p>saldo liquido 90.15</p>       
+      <article className="cuenta background">
+        <h1 className="mr">cuenta</h1>
+        <h3>salgo liquido: ${(invertido).toFixed(2)}</h3>
+        <h3>salgo stakeado: ${(stakeado).toFixed(2)}</h3>
+        <p>
+          <code>Total : ${(invertido + stakeado).toFixed(2)}</code>
+        </p>
+        <button
+  onClick={() => {
+    if (window.confirm("¿Estás seguro de invertir el total?")) {
+      setStakeado(stakeado + 80.4);
+      setInvertido(invertido - 80.4);
+    }
+  }}
+>
+  Invertir total
+</button>
       </article>
     </>
   );

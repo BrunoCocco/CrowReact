697cf139f102f9b68be494fc7994376f1e6b0ef6
Bruno Cocco <brunoe.cocco@gmail.com>
2025-11-15 11:22:18 +0100

    cambios de estilo

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <>
+      <button>boton 1</button>
+      <button>boton 3</button>
+      <button>boton2</button>
+    </>
+  );
+}
+
+export default Header;
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,16 +1,19 @@
-import "./inicio.css"
+import { useState } from "react";
+import "./components.css";
 import Cuenta from "../layouts/Cuenta";
-import Proyectos from "../layouts/Proyecto";
+import Proyecto from "../layouts/Proyecto";
 import Main from "../layouts/Main";
 
 function Inicio() {
+  const [invertido, setInvertido] = useState(0);
+  const [stakeado, setStakeado] = useState(0)
   return (
     <>
-    <section className="container">
-      <Cuenta />
-      <Proyectos />
-      <Main />
-    </section>
+      <section className="container">
+        <Cuenta invertido={invertido} setInvertido={setInvertido} stakeado={stakeado} setStakeado={setStakeado} />
+        <Proyecto invertido={invertido} setInvertido={setInvertido} />
+        <Main stakeado={stakeado} setStakeado={setStakeado}/>
+      </section>
     </>
   );
 }
